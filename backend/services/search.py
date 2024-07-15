6a03d26b6009359b8591af3783614e1abf29fa3e
simonweigold <simonw750@gmail.com>
2024-07-15 15:18:07 +0200

    fix structure of results

--- backend/services/search.py
+++ backend/services/search.py
@@ -11,13 +11,30 @@ class SearchService:
 
     def basic_search(self, search_string):
         all_entries = self.db.get_all_entries()
-        results, total_matches = {}, 0
+        results = {}
+        total_matches = 0
+        search_terms = search_string.lower().split()
 
         for table, entries in all_entries.items():
-            matching_entries = [entry for entry in entries if any(search_term.lower() in str(value).lower() for search_term in search_string.split() for value in entry.values())]
+            matching_entries = [
+                entry for entry in entries
+                if all(any(search_term in str(value).lower() for value in entry.values()) for search_term in search_terms)
+            ]
             if matching_entries:
-                results[table] = matching_entries
+                results[table] = {
+                    'matches': len(matching_entries),
+                    'results': matching_entries
+                }
                 total_matches += len(matching_entries)
 
-        final_results = {'total_matches': total_matches, 'tables': results}
+        final_results = {
+            'total_matches': total_matches,
+            'tables': results
+            }
         return filter_na(parse_results(final_results))
+
+    def filtered_search(self, search_string, filter_string):
+        return "foo"
+
+    def semantic_search(self, search_string):
+        return "foo"
