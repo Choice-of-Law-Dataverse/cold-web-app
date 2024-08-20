06d7d8fd217d2053b9f7c84630ee01c27b654a76
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-08-20 17:23:51 +0200

    Setup Nuxt

---
+++ frontend/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import SearchComponent from '~/components/SearchComponent.vue';
+</script>
+
+<template>
+    <div style="display: flex; align-items: center;">
+      <SearchComponent />
+    </div>
+  </template>
+  
