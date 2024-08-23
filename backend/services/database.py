126f26d0b302929d1d7ead473730608fe6b46582
simonweigold <simonw750@gmail.com>
2024-08-23 15:24:02 +0200

    include error handling

--- backend/services/database.py
+++ backend/services/database.py
@@ -1,14 +1,24 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 
 class Database:
     def __init__(self, connection_string):
         self.engine = sa.create_engine(f"mssql+pyodbc:///?odbc_connect={connection_string}")
         self.metadata = sa.MetaData()
-        self.metadata.reflect(bind=self.engine)
+
+        try:
+            self.metadata.reflect(bind=self.engine)
+        except SQLAlchemyError as e:
+            print(f"Error reflecting metadata: {e}")
+            self.metadata = None
+
         self.session = sessionmaker(bind=self.engine)()
 
     def get_all_entries(self):
+        if not self.metadata:
+            return None
+        
         all_entries = {}
         try:
             for table_name, table in self.metadata.tables.items():
@@ -17,6 +27,9 @@ class Database:
                 columns = result.keys()
                 entries = [dict(zip(columns, row)) for row in result.fetchall()]
                 all_entries[table_name] = entries
+        except SQLAlchemyError as e:
+            print(f"Error getting all entries: {e}")
+            return None
         finally:
             self.session.close()
         return all_entries
