aa2240acb67268841ab4de202561d771a0db09cd
simonweigold <simonw750@gmail.com>
2024-08-26 16:57:33 +0200

    import dummy conn string

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    AZURE_POSTGRESQL_DUMMY_CONN_STRING = os.getenv("AZURE_POSTGRESQL_DUMMY_CONN_STRING")
     AZURE_SQL_CONNECTION_STRING = os.getenv("AZURE_SQL_CONNECTION_STRING")
     MIXEDBREAD_API_KEY = os.getenv("MIXEDBREAD_API_KEY")
     TEST = os.getenv("TEST")
