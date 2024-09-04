ded53fcb3083875dc8f5c7240ac9ddf5fa181f71
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-09-04 18:10:24 +0200

    Init MainHeader above search input

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import SearchComponent from '~/components/SearchComponent.vue';
+import MainHeader from '~/components/MainHeader.vue';
 </script>
 
 <template>
+  <div style="display: flex; align-items: center;">
+      <MainHeader />
+    </div>
     <div style="display: flex; align-items: center;">
       <SearchComponent />
     </div>
