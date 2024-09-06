e3a34c0424b3a0d8c8b2d719f4e4a220f8554cdc
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-09-06 15:45:17 +0200

    Add placeholder translation toggle

--- frontend/components/LegalProvisionModal.vue
+++ frontend/components/LegalProvisionModal.vue
@@ -22,9 +22,12 @@
           <p class="result-key">ARTICLE</p>
           <p class="result-value">Art. 116</p>
   
-          <p class="result-key">FULL TEXT OF THE PROVISION</p>
-          <p class="result-value">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then
-          </p>
+          <!-- Container with flexbox for aligned content -->
+           <div class="provision-container">
+            <p class="result-key">FULL TEXT OF THE PROVISION</p>
+            <p class="result-right">Translation <UToggle size="2xs" v-model="selected" /> Original</p>
+          </div>
+          <p class="result-value">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
       </UCard>
     </UModal>
   </div>
@@ -32,6 +35,18 @@
 
 <style scoped>
 
+.provision-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.result-right {
+  margin-left: auto; /* Push the right-aligned text to the far right */
+  text-align: right;
+  font-size: x-small;
+  font-weight: bold;
+}
 
 .close-link {
   position: absolute;
@@ -75,6 +90,8 @@
 <script setup lang="ts">
 
 import { defineEmits } from 'vue'
+
+const selected = ref(false)
 
 // Accept `isVisible` prop from the parent component
 const props = defineProps({
