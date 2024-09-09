856f9e0397c163bff49eaa613d6412fdcc3448f3
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-09-09 11:29:58 +0200

    Init Search Suggestion buttons with placeholder text

---
+++ frontend/components/SearchSuggestions.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h3>Search Suggestions:</h3>
+      <div class="suggestions-container">
+        <div v-for="(suggestion, index) in searchSuggestions" :key="index" class="suggestion-button">
+          <UButton
+            color="primary"
+            variant="soft"
+            size="lg"
+          >
+            {{ suggestion }}
+          </UButton>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        searchSuggestions: ["Search Suggestion 1", "Search Suggestion 2", "Search Suggestion 3"]
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+
+h3 {
+    font-size: medium;
+    margin-bottom: 1em;
+    margin-top: 1em;
+    text-align: center; /* Center the text */
+  }
+
+  .suggestions-container {
+    display: flex;
+    gap: 10px; /* Adds space between buttons */
+  }
+  
+  .suggestion-button {
+    flex-shrink: 0; /* Prevents the buttons from shrinking */
+  }
+  </style>
+  
