b79f79c85cfbe3f7764e4e40ba401265c45f8462
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-10-04 14:38:37 +0200

    Make code pretty

--- frontend/components/CourtDecisionModal.vue
+++ frontend/components/CourtDecisionModal.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
-    <!-- Button to trigger modal -->
-    <!-- <UButton label="Open CourtDecisionsModal" @click="isOpen = true" /> -->
-    
     <!-- Modal with custom width and centered positioning -->
-    <UModal :modelValue="isVisible" :ui="{ width: 'w-full sm:max-w-4xl' }" prevent-close @close="emitClose">
-      <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+    <UModal
+      :modelValue="isVisible"
+      :ui="{ width: 'w-full sm:max-w-4xl' }"
+      prevent-close
+      @close="emitClose"
+    >
+      <UCard
+        :ui="{
+          ring: '',
+          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+        }"
+      >
         <template #header>
           <div class="flex items-center justify-between">
-            <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
-              {{ data?.Case || '[Missing Title]' }} <!-- Display the Name value from data or a placeholder -->
+            <h3
+              class="text-base font-semibold leading-6 text-gray-900 dark:text-white"
+            >
+              {{ data?.Case || '[Missing Title]' }}
+              <!-- Display the Name value from data or a placeholder -->
             </h3>
-            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="emitClose" />
+            <UButton
+              color="gray"
+              variant="ghost"
+              icon="i-heroicons-x-mark-20-solid"
+              class="-my-1"
+              @click="emitClose"
+            />
           </div>
         </template>
 
@@ -19,38 +35,68 @@
 
         <!-- Display greyed out PDF download link until URL issue is fixed -->
         <div class="modal-content">
-          <span class="icon-wrapper" style="color: grey; opacity: 0.5;">
+          <span class="icon-wrapper" style="color: grey; opacity: 0.5">
             <UIcon name="i-material-symbols:file-open" />
           </span>
-          <a class="result-value" style="color: grey; opacity: 0.5; cursor: not-allowed;">View original PDF<br><br>
+          <a
+            class="result-value"
+            style="color: grey; opacity: 0.5; cursor: not-allowed"
+            >View original PDF<br /><br />
           </a>
 
           <p class="result-key">JURISDICTION</p>
-          <p class="result-value">{{ data?.['Jurisdiction Names'] || '[Missing Jurisdiction]' }}</p>
-  
+          <p class="result-value">
+            {{ data?.['Jurisdiction Names'] || '[Missing Jurisdiction]' }}
+          </p>
+
           <p class="result-key">ABSTRACT</p>
-          <p class="result-value">{{ data?.Abstract || '[No Abstract available]' }}</p>
-  
+          <p class="result-value">
+            {{ data?.Abstract || '[No Abstract available]' }}
+          </p>
+
           <p class="result-key">RELEVANT FACTS / SUMMARY OF THE CASE</p>
-          <p class="result-value">{{ data?.['Relevant facts / Summary of the case'] || '[Missing Relevant Facts or Summary]' }}</p>
+          <p class="result-value">
+            {{
+              data?.['Relevant facts / Summary of the case'] ||
+              '[Missing Relevant Facts or Summary]'
+            }}
+          </p>
 
           <p class="result-key">RELEVANT RULES OF LAW INVOLVED</p>
-          <p class="result-value">{{ data?.['Relevant rules of law involved'] || '[Missing Relevant Rules]' }}</p>
+          <p class="result-value">
+            {{
+              data?.['Relevant rules of law involved'] ||
+              '[Missing Relevant Rules]'
+            }}
+          </p>
 
           <p class="result-key">CHOICE OF LAW ISSUE</p>
-          <p class="result-value">{{ data?.['Choice of law issue'] || '[Missing Choice of Law Issue]' }}</p>
+          <p class="result-value">
+            {{
+              data?.['Choice of law issue'] || '[Missing Choice of Law Issue]'
+            }}
+          </p>
 
           <p class="result-key">COURT'S POSITION</p>
-          <p class="result-value">{{ data?.["Court's position"] || "[Missing Court's Positions]" }}</p>
+          <p class="result-value">
+            {{ data?.["Court's position"] || "[Missing Court's Positions]" }}
+          </p>
 
           <p class="result-key">TEXT OF THE RELEVANT LEGAL PROVISIONS</p>
-          <p class="result-value">{{ data?.['Text of the relevant legal provisions'] || "[Missing Text of Relevant Legal Provisions]" }}</p>
+          <p class="result-value">
+            {{
+              data?.['Text of the relevant legal provisions'] ||
+              '[Missing Text of Relevant Legal Provisions]'
+            }}
+          </p>
 
           <p class="result-key">QUOTE</p>
-          <p class="result-value">{{ data?.['Quote'] || "[Missing Quote]" }}</p>
+          <p class="result-value">{{ data?.['Quote'] || '[Missing Quote]' }}</p>
 
           <p class="result-key">TRANSLATED EXCERPT</p>
-          <p class="result-value">{{ data?.['Translated excerpt'] || "[Missing Translated Excerpt]" }}</p>
+          <p class="result-value">
+            {{ data?.['Translated excerpt'] || '[Missing Translated Excerpt]' }}
+          </p>
         </div>
       </UCard>
     </UModal>
@@ -58,26 +104,19 @@
 </template>
 
 <script setup lang="ts">
+// Define props and emit in a more concise way
+const { isVisible, data } = defineProps<{
+  isVisible: boolean
+  data: Record<string, any>
+}>()
 
-// Accept `isVisible` prop from the parent component
-const props = defineProps({
-  isVisible: Boolean,
-  data: Object  // Define the data prop to accept an object
-})
-
-// Emit event for closing the modal
 const emit = defineEmits(['close'])
 
-// Function to emit the close event
-function emitClose() {
-  emit('close')
-}
-
+// Emit the close event
+const emitClose = () => emit('close')
 </script>
 
-
 <style scoped>
-
 .close-link {
   position: absolute;
   top: -20px;
@@ -89,30 +128,30 @@ function emitClose() {
 }
 
 .icon-wrapper {
-    margin-right: 4px;
-    margin-top: 5px;
+  margin-right: 4px;
+  margin-top: 5px;
 }
-  
-  .result-item {
-    margin-bottom: 1rem;
-  }
-  
-  .result-key {
-    font-size: x-small;
-    font-weight: bold;
-  }
-  
-  /* Adding styles to ensure text wrapping */
-  .result-value {
-    word-wrap: break-word; /* Allows breaking within words if necessary */
-    word-break: break-word; /* Breaks words that are too long */
-    white-space: pre-wrap; /* Preserves whitespace and line breaks, but also allows wrapping */
-    margin-bottom: 2em;
-  }
 
-  a {
-    text-decoration: underline;
-    text-underline-offset: 6px;
-    text-decoration-thickness: 1px;
-  }
-  </style>
+.result-item {
+  margin-bottom: 1rem;
+}
+
+.result-key {
+  font-size: x-small;
+  font-weight: bold;
+}
+
+/* Adding styles to ensure text wrapping */
+.result-value {
+  word-wrap: break-word; /* Allows breaking within words if necessary */
+  word-break: break-word; /* Breaks words that are too long */
+  white-space: pre-wrap; /* Preserves whitespace and line breaks, but also allows wrapping */
+  margin-bottom: 2em;
+}
+
+a {
+  text-decoration: underline;
+  text-underline-offset: 6px;
+  text-decoration-thickness: 1px;
+}
+</style>
--- frontend/components/LegalProvisionModal.vue
+++ frontend/components/LegalProvisionModal.vue
@@ -1,74 +1,93 @@
 <template>
   <div>
-    
     <!-- Modal with custom width and centered positioning -->
-    <UModal :modelValue="isVisible" :ui="{ width: 'w-full sm:max-w-4xl' }" prevent-close @close="emitClose">
-      <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+    <UModal
+      :modelValue="isVisible"
+      :ui="{ width: 'w-full sm:max-w-4xl' }"
+      prevent-close
+      @close="emitClose"
+    >
+      <UCard
+        :ui="{
+          ring: '',
+          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+        }"
+      >
         <template #header>
           <div class="flex items-center justify-between">
-            <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
-              {{ data?.Name || '[Missing Name]' }} <!-- Display the Name value from data or a placeholder -->
+            <h3
+              class="text-base font-semibold leading-6 text-gray-900 dark:text-white"
+            >
+              {{ data?.Name || '[Missing Name]' }}
+              <!-- Display the Name value from data or a placeholder -->
             </h3>
-            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="emitClose" />
+            <UButton
+              color="gray"
+              variant="ghost"
+              icon="i-heroicons-x-mark-20-solid"
+              class="-my-1"
+              @click="emitClose"
+            />
           </div>
         </template>
 
-        <!-- Modal Content -->  
-          <p class="result-key">JURISDICTION</p>
-          <p class="result-value">{{ data?.['Jurisdiction name'] || '[Missing Jurisdiction]' }}</p>
-  
-          <p class="result-key">ARTICLE</p>
-          <p class="result-value">{{ data?.Article || '[Missing Article]' }}</p>
-  
-          <!-- Container with flexbox for aligned content -->
-          <div class="provision-container">
-            <p class="result-key">FULL TEXT OF THE PROVISION</p>
-            <!-- Container for "Display:" and the select element -->
-             <div class="right-container">
-              <!-- <p class="display-label">Display:</p> -->
-              <USelect
+        <!-- Modal Content -->
+        <p class="result-key">JURISDICTION</p>
+        <p class="result-value">
+          {{ data?.['Jurisdiction name'] || '[Missing Jurisdiction]' }}
+        </p>
+
+        <p class="result-key">ARTICLE</p>
+        <p class="result-value">{{ data?.Article || '[Missing Article]' }}</p>
+
+        <!-- Container with flexbox for aligned content -->
+        <div class="provision-container">
+          <p class="result-key">FULL TEXT OF THE PROVISION</p>
+          <!-- Container for "Display:" and the select element -->
+          <div class="right-container">
+            <!-- <p class="display-label">Display:</p> -->
+            <USelect
               size="2xs"
               variant="none"
               :options="['Original Language', 'English Translation']"
-              v-model="selectedLanguage" 
-              />
-            </div>
+              v-model="selectedLanguage"
+            />
           </div>
-          <!-- Conditionally display the full text based on the selected language -->
-           <p class="result-value">
-            {{ selectedLanguage === 'Original Language' 
-            ? (data?.['Full text of the provision (Original language)'] || '[Missing Text]') 
-            : (data?.['Full text of the provision (English translation)'] || '[Missing Text]') 
-            }}
-            </p>
-        </UCard>
+        </div>
+        <!-- Conditionally display the full text based on the selected language -->
+        <p class="result-value">
+          {{
+            selectedLanguage === 'Original Language'
+              ? data?.['Full text of the provision (Original language)'] ||
+                '[Missing Text]'
+              : data?.['Full text of the provision (English translation)'] ||
+                '[Missing Text]'
+          }}
+        </p>
+      </UCard>
     </UModal>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 
+// Define reactive variables
 const selected = ref(false)
-const selectedLanguage = ref('Original Language'); // Used for Provision Language toggle
+const selectedLanguage = ref('Original Language')
 
-// Accept `isVisible` prop from the parent component
-const props = defineProps({
-  isVisible: Boolean,
-  data: Object  // Define the data prop to accept an object
-})
+// Destructure props directly
+const { isVisible, data } = defineProps<{
+  isVisible: boolean
+  data: Record<string, any>
+}>()
 
-// Emit event for closing the modal
+// Emit the close event using arrow function
 const emit = defineEmits(['close'])
-
-// Function to emit the close event
-function emitClose() {
-  emit('close')
-}
-
+const emitClose = () => emit('close')
 </script>
 
 <style scoped>
-
 .provision-container {
   display: flex;
   justify-content: space-between;
@@ -98,30 +117,30 @@ function emitClose() {
 }
 
 .icon-wrapper {
-    margin-right: 4px;
-    margin-top: 5px;
+  margin-right: 4px;
+  margin-top: 5px;
 }
-  
-  .result-item {
-    margin-bottom: 1rem;
-  }
-  
-  .result-key {
-    font-size: x-small;
-    font-weight: bold;
-  }
-  
-  /* Adding styles to ensure text wrapping */
-  .result-value {
-    word-wrap: break-word; /* Allows breaking within words if necessary */
-    word-break: break-word; /* Breaks words that are too long */
-    white-space: pre-wrap; /* Preserves whitespace and line breaks, but also allows wrapping */
-    margin-bottom: 2em;
-  }
 
-  a {
-    text-decoration: underline;
-    text-underline-offset: 6px;
-    text-decoration-thickness: 1px;
-  }
-  </style>
+.result-item {
+  margin-bottom: 1rem;
+}
+
+.result-key {
+  font-size: x-small;
+  font-weight: bold;
+}
+
+/* Adding styles to ensure text wrapping */
+.result-value {
+  word-wrap: break-word; /* Allows breaking within words if necessary */
+  word-break: break-word; /* Breaks words that are too long */
+  white-space: pre-wrap; /* Preserves whitespace and line breaks, but also allows wrapping */
+  margin-bottom: 2em;
+}
+
+a {
+  text-decoration: underline;
+  text-underline-offset: 6px;
+  text-decoration-thickness: 1px;
+}
+</style>
