febee0ef1e71ded8e7cb3e619a7579c45d11a815
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-10-07 15:41:34 +0200

    Init Helper Chatbot in front end

---
+++ frontend/components/HelperChatbot.vue
@@ -0,0 +1,21 @@
+<template>
+  <UContainer
+    style="
+      margin-top: 50px;
+      margin-bottom: 50px;
+      width: 800px;
+      max-width: 1200px;
+      margin-left: auto;
+      margin-right: auto;
+    "
+  >
+    <h2>Helper Chatbot Response</h2>
+    <UCard
+      :style="{ backgroundColor: '#f5f5f5', boxShadow: 'none', border: 'none' }"
+    >
+      <p>Hello, I'm your Helper Chatbot</p>
+    </UCard>
+  </UContainer>
+</template>
+
+<script></script>
