690ae79bd4fc0d1c23b465f808f32a6616650283
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-10-25 17:55:48 +0200

    Init search page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-import SearchComponent from '~/components/SearchComponent.vue';
-import MainHeader from '~/components/MainHeader.vue';
+// import SearchComponent from '~/components/SearchComponent.vue'
+// import MainHeader from '~/components/MainHeader.vue'
 </script>
 
 <template>
-  <div style="display: flex; align-items: center;">
-      <MainHeader />
-    </div>
-    <div style="display: flex; align-items: center;">
-      <SearchComponent />
-    </div>
-  </template>
-  
+  <!-- <div style="display: flex; align-items: center">
+    <MainHeader />
+  </div>
+  <div style="display: flex; align-items: center">
+    <SearchComponent />
+  </div> -->
+  <p>Choice of Law Dataverse</p>
+  <p>Find relevant case law and choice of law answers for your case.</p>
+</template>
---
+++ frontend/pages/search.vue
@@ -0,0 +1,8 @@
+<template>
+  <div style="display: flex; align-items: center">
+    <p>Search results</p>
+    <div v-if="results && !noResults">
+      <SearchResults :data="results" />
+    </div>
+  </div>
+</template>
