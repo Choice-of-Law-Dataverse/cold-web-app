7ac7383120ca63a4c5180884cd799aae194c4be3
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-10-29 10:50:26 +0100

    Init column styling and apply to PopularSearches

--- frontend/components/PopularSearches.vue
+++ frontend/components/PopularSearches.vue
@@ -1,15 +1,19 @@
 <template>
-  <UCard>
-    <p>Popular Searches</p>
-    <div v-for="(suggestion, index) in searchSuggestions" :key="index">
-      <NuxtLink
-        :to="`/search?q=${formatQuery(suggestion)}`"
-        class="suggestion-link"
-      >
-        {{ suggestion }}
-      </NuxtLink>
+  <div class="container">
+    <div class="col-span-4">
+      <UCard>
+        <p>Popular Searches</p>
+        <div v-for="(suggestion, index) in searchSuggestions" :key="index">
+          <NuxtLink
+            :to="`/search?q=${formatQuery(suggestion)}`"
+            class="suggestion-link"
+          >
+            {{ suggestion }}
+          </NuxtLink>
+        </div>
+      </UCard>
     </div>
-  </UCard>
+  </div>
 </template>
 
 <script>
