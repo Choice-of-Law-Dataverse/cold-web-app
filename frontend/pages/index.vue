963ecaee7d301f32673af8d7e07836621e31ca36
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-10-29 10:51:37 +0100

    Apply column style

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-// import SearchComponent from '~/components/SearchComponent.vue'
-// import MainHeader from '~/components/MainHeader.vue'
 import PopularSearches from '~/components/PopularSearches.vue'
 </script>
 
 <template>
-  <!-- <div style="display: flex; align-items: center">
-    <MainHeader />
-  </div> -->
-  <div style="display: flex; align-items: center">
-    <!-- <SearchComponent /> -->
+  <div class="container">
+    <div class="col-span-4">
+      <p>Choice of Law Dataverse</p>
+      <p>Find relevant case law and choice of law answers for your case.</p>
+    </div>
   </div>
-  <p>Choice of Law Dataverse</p>
-  <p>Find relevant case law and choice of law answers for your case.</p>
   <PopularSearches />
 </template>
