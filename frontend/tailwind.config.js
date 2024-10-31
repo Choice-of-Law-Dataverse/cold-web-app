e1b63d63a0f51dacec5f2351457903bee9a59c70
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-10-31 08:10:05 +0100

    Add source table label colours

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,13 +15,22 @@ export default {
         'cold-night': '#0F0035',
         'cold-night-alpha': 'rgba(15, 0, 53, 0.5)', // 50% alpha
         'cold-black': '#262626',
+        
         'cold-bg': '#FAFAFA',
         'cold-gray': '#E2E8F0',
-        'cold-gray-alpha': 'rgba(226, 232, 240, 0.5)', // 50% alpha
+        'cold-gray-alpha': '#E2E8F080', // 50% alpha
+        
         'label-question': '#FFA8FB',
+        'label-question-alpha': '#FFA8FB1A', // 10% alpha
+        
         'label-court-decision': '#FF7167',
+        'label-court-decision-alpha': '#FF71671A', // 10% alpha
+        
         'label-legal-instrument': '#FF9D00',
+        'label-legal-instrument-alpha': '#FF9D001A', // 10% alpha
+        
         'label-literature': '#4DC3FA',
+        'label-literature-alpha': '#4DC3FA1A', // 10% alpha
       },
     },
   },
@@ -35,12 +44,21 @@ export default {
           '--color-cold-purple': theme('colors.cold-purple'),
           '--color-cold-night': theme('colors.cold-night'),
           '--color-cold-night-alpha': theme('colors.cold-night-alpha'),
+          
           '--color-cold-gray': theme('colors.cold-gray'),
           '--color-cold-gray-alpha': theme('colors.cold-gray-alpha'),
+          
           '--color-label-question': theme('colors.label-question'),
+          '--color-label-question-alpha': theme('colors.label-question-alpha'),
+          
           '--color-label-court-decision': theme('colors.label-court-decision'),
+          '--color-label-court-decision-alpha': theme('colors.label-court-decision-alpha'),
+          
           '--color-label-legal-instrument': theme('colors.label-legal-instrument'),
+          '--color-label-legal-instrument-alpha': theme('colors.label-legal-instrument-alpha'),
+          
           '--color-label-literature': theme('colors.label-literature'),
+          '--color-label-literature-alpha': theme('colors.label-literature-alpha'),
         },
       })
     },
