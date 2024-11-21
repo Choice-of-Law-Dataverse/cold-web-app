8ee01bff1ebb3a2456142e51b96726c92f3c8748
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-11-21 11:00:58 +0100

    Fix spacing issue between components on detail page

---
+++ frontend/components/JurisdictionComparison.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- <div class="container"> -->
+  <div class="col-span-12">
+    <UCard class="cold-ucard">
+      <h2 class="popular-title">Jurisdiction Comparison</h2>
+    </UCard>
+  </div>
+  <!-- </div> -->
+</template>
