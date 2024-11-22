41e110b9919a7b584499acf91a0b0870f6ca7a2f
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-11-22 16:27:32 +0100

    First attempt to style select dropdown

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -6,6 +6,9 @@ export default defineAppConfig({
         ring: 'ring-1 ring-[var(--color-cold-gray)]', // Add border with custom color
         divide: 'divide-y divide-[var(--color-cold-gray)]', // Override divider color
       },
+      select: {
+        rounded: 'rounded-none',       // Remove border radius
+       },
       primary: 'cool',
       gray: 'cool'
     }
