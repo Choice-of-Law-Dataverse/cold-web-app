ea1bebdede3290139c4210c6a2c6efa75d36b9f3
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-11-26 09:42:00 +0100

    Update map

--- frontend/components/JurisdictionMap.vue
+++ frontend/components/JurisdictionMap.vue
@@ -1,22 +1,25 @@
 <template>
-  <div style="height: 100vh; width: 100vw">
-    <LMap
-      ref="map"
-      :zoom="zoom"
-      :center="[47.21322, -1.559482]"
-      :use-global-leaflet="false"
-    >
-      <LTileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&amp;copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-        layer-type="base"
-        name="OpenStreetMap"
-      />
-    </LMap>
-  </div>
+  <UCard class="cold-ucard">
+    <div class="popular-searches-container">
+      <h2 class="popular-title">Number of Court Decisions</h2>
+      <div style="height: 100vh; width: 100vw">
+        <!-- Leaflet Map -->
+        <LMap
+          :zoom="13"
+          :center="[51.505, -0.09]"
+          style="height: 100%; width: 100%"
+        >
+          <!-- Tile Layer -->
+          <LTileLayer
+            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            :attribution="'&copy; OpenStreetMap contributors'"
+          />
+        </LMap>
+      </div>
+    </div>
+  </UCard>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const zoom = ref(6)
+import 'leaflet/dist/leaflet.css'
 </script>
