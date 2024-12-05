800b2cbbff8dd574430e65da6fba0a8df4de5ab4
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-12-05 10:55:47 +0100

    Add slider to Overview

--- frontend/pages/about/Overview.vue
+++ frontend/pages/about/Overview.vue
@@ -1,13 +1,29 @@
 <template>
+  <UCarousel
+    :items="items"
+    :ui="{ item: 'basis-full' }"
+    class="rounded-lg overflow-hidden"
+    arrows
+    indicators
+  >
+    <template v-slot="{ item }">
+      <component :is="item" />
+    </template>
+  </UCarousel>
   <div class="copy" v-html="htmlContent"></div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { marked } from 'marked' // Import the Markdown parser
+import AboutRepo from './slider/AboutRepo.vue'
+import AboutCommunity from './slider/AboutCommunity.vue'
+import AboutOpenResearch from './slider/AboutOpenResearch.vue'
 
 const content = ref('') // Reactive variable for storing the file content
 const htmlContent = ref('') // Store parsed HTML content
+
+const items = [AboutRepo, AboutCommunity, AboutOpenResearch]
 
 onMounted(async () => {
   try {
