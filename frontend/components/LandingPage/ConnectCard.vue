a5cfcb3ca8afc3603f322ad216a8db77decb65b4
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-01-17 09:21:20 +0100

    Init connect card

---
+++ frontend/components/LandingPage/ConnectCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <UCard class="cold-ucard">
+    <h2 class="popular-title">CoLD Newsletter</h2>
+    <div class="icon-container">
+      <Icon name="i-bi:substack" size="96" style="color: #ff6719" />
+    </div>
+    <div class="link-container">
+      <UButton
+        class="suggestion-button"
+        variant="link"
+        icon="i-material-symbols:arrow-forward"
+        trailing
+      >
+        Subscribe on Substack
+      </UButton>
+    </div>
+  </UCard>
+</template>
+
+<style scoped>
+h2 {
+  text-align: center;
+}
+
+.icon-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 32px;
+}
+
+.link-container {
+  display: flex;
+  justify-content: center;
+}
+</style>
