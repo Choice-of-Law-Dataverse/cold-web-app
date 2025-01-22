7f4bab78441f2837cdf5aa16ef80fd809b6366de
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-01-22 13:20:03 +0100

    Add responsiveness to Contact and Disclaimer

--- frontend/pages/disclaimer.vue
+++ frontend/pages/disclaimer.vue
@@ -11,25 +11,3 @@
     </div>
   </main>
 </template>
-
-<style scoped>
-.main-content-grid {
-  display: grid;
-  grid-template-columns: repeat(12, minmax(0, 1fr)); /* 12-column layout */
-  column-gap: var(--gutter-width); /* Gutter space between columns */
-  padding: 32px; /* Optional padding to match the card's interior padding */
-}
-
-.grid-item {
-  grid-column: 1 / span 6; /* Start in the 1st column, span across 6 columns */
-  margin-bottom: 48px; /* Space between each key-value pair */
-}
-
-::v-deep(p) {
-  margin-bottom: 24px;
-}
-
-::v-deep(h2) {
-  margin-bottom: 18px;
-}
-</style>
