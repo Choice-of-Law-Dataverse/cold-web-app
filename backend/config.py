3896cccd17601a59a89b7c0d74fa9e6ce8f6bf4d
simonweigold <simonw750@gmail.com>
2025-01-23 20:08:56 +0100

    Fix variable import

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Config:
-    SQL_CONN_STRING = os.getenv("SQL_CONN_STRING_PLAYGROUND")
+    SQL_CONN_STRING = os.getenv("SQL_CONN_STRING")
     MIXEDBREAD_API_KEY = os.getenv("MIXEDBREAD_API_KEY")
     TEST = os.getenv("TEST")
     MONGODB_CONN_STRING = os.getenv("MONGODB_CONN_STRING")
