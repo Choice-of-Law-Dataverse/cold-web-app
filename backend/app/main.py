6ef4f2963c7d687edac9daa3ff0297bd7960c04c
Simon Weigold <simonw750@gmail.com>
2025-02-10 22:43:43 +0100

    Add submarine easteregg

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth import verify_jwt_token
-from app.routes import ai, search, user
+from app.routes import ai, search, submarine, user
 from app.services.query_logging import log_query
 
 app = FastAPI()
@@ -22,6 +22,7 @@ app.middleware("http")(log_query)
 app.include_router(search.router)
 app.include_router(user.router)
 app.include_router(ai.router)
+app.include_router(submarine.router)
 
 
 @app.get("/", dependencies=[Depends(verify_jwt_token)])
