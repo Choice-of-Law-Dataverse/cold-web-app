b7f5e0c5bf70aa18e963160dffd792bf9f9f787b
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-02-13 13:32:18 +0100

    Create component for question source

---
+++ frontend/components/QuestionSourceList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li v-for="(source, index) in sources" :key="index" :class="valueClass">
+      <LegalProvisionRenderer
+        :value="source"
+        :fallbackData="fallbackData"
+        :valueClassMap="valueClassMap"
+      />
+    </li>
+  </ul>
+</template>
+
+<script setup>
+defineProps({
+  sources: Array,
+  fallbackData: Object,
+  valueClassMap: Object,
+  valueClass: {
+    type: String,
+    default: 'result-value-medium',
+  },
+})
+</script>
