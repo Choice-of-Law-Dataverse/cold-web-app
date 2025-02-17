73a8a26e049f61b3f347b599bb9ae12debf26b39
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-02-17 09:25:51 +0100

    Init Notification Banner

---
+++ frontend/components/NotificationBanner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="notification-banner result-value-small">
+    We don’t have a lot of data for this jurisdiction.
+    <a href="/contact" class="contact-link">Contact us</a> if you want to
+    contribute.
+  </div>
+</template>
+
+<style scoped>
+.notification-banner {
+  @apply bg-[--color-cold-cream] p-4;
+}
+</style>
