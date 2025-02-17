ee45e90086dad553759da8886500ddfa4edf14d8
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-02-17 09:43:15 +0100

    Adjust styling

--- frontend/components/NotificationBanner.vue
+++ frontend/components/NotificationBanner.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="notification-banner result-value-small">
+    <UIcon
+      name="i-material-symbols:info-outline"
+      size="16"
+      class="icon-adjust"
+    />
     We don’t have a lot of data for this jurisdiction.
     <a href="/contact" class="contact-link">Contact us</a> if you want to
     contribute.
@@ -8,6 +13,12 @@
 
 <style scoped>
 .notification-banner {
-  @apply bg-[--color-cold-cream] p-4;
+  @apply bg-[--color-cold-cream] p-4 outline outline-1 outline-[--color-cold-cream];
+  margin-bottom: 24px;
+}
+
+.icon-adjust {
+  @apply translate-y-[2px];
+  margin-right: 4px;
 }
 </style>
