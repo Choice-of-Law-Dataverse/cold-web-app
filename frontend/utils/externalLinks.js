7e8eb01c08783e35aa4e1d8cc4b40e85d18acc00
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-02-19 10:18:58 +0100

    Add beta user connect card

--- frontend/utils/externalLinks.js
+++ frontend/utils/externalLinks.js
@@ -2,5 +2,6 @@ export const externalLinks = {
     substack: 'https://choiceoflawdataverse.substack.com/subscribe',
     linkedin: 'https://www.linkedin.com/company/choice-of-law-dataverse',
     email: 'mailto:mail@cold.global',
+    beta: 'https://airtable.com/appIVwL92qjH07w0R/pagbSedpAvej7HsNu/form',
   };
   
