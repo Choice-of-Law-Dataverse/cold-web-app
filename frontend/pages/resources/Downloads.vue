40afb9f35bb3552a1dc7d4eabdef5ad3d322dfad
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-02-19 09:34:57 +0100

    Populate downloads page

--- frontend/pages/resources/Downloads.vue
+++ frontend/pages/resources/Downloads.vue
@@ -1,1 +1,36 @@
-<template>Downloads</template>
+<template>
+  <h2>Downloads</h2>
+  <li>
+    All court decisions (<a
+      href="https://choiceoflawdataverse.blob.core.windows.net/full-tables/court-decisions.csv"
+      >CSV</a
+    >
+    |
+    <a
+      href="https://choiceoflawdataverse.blob.core.windows.net/full-tables/court-decisions.xlsx"
+      >XLSX</a
+    >)
+  </li>
+  <li>
+    All legislations (<a
+      href="https://choiceoflawdataverse.blob.core.windows.net/full-tables/legislations.csv"
+      >CSV</a
+    >
+    |
+    <a
+      href="https://choiceoflawdataverse.blob.core.windows.net/full-tables/legislations.xlsx"
+      >XLSX</a
+    >)
+  </li>
+  <li>
+    All literature entries (<a
+      href="https://choiceoflawdataverse.blob.core.windows.net/full-tables/literature.csv"
+      >CSV</a
+    >
+    |
+    <a
+      href="https://choiceoflawdataverse.blob.core.windows.net/full-tables/literature.xlsx"
+      >XLSX</a
+    >)
+  </li>
+</template>
