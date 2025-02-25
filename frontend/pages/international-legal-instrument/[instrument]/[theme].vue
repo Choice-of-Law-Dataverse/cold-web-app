d5412e1751e9d12aa3233024b5f1721b2f4efc28
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-02-25 12:04:26 +0100

    Adjust USelectMenu position

--- frontend/pages/international-legal-instrument/[instrument]/[theme].vue
+++ frontend/pages/international-legal-instrument/[instrument]/[theme].vue
@@ -26,7 +26,7 @@
               <!-- Right Column: Select Instrument + Comparison Full Text -->
               <div>
                 <!-- Move USelectMenu to the top -->
-                <div class="mb-4 flex justify-end">
+                <div class="ml-[-10px] mt-[9px]">
                   <USelectMenu
                     placeholder="Select International Instrument"
                     v-model="selectedInstrument"
