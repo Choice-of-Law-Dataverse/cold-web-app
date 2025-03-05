ef3bcb0ac374e5de92ea37cc0df0631a28633d91
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-03-05 18:38:11 +0100

    Implement "Show more Related Literature" on Jurisdiction pages

--- frontend/components/RelatedLiterature.vue
+++ frontend/components/RelatedLiterature.vue
@@ -4,16 +4,43 @@
 
     <!-- If we're using literature ID mode -->
     <template v-if="useId">
+      <!-- While waiting for literature IDs to load, you might show a loading state if needed -->
       <ul>
         <li
-          v-for="(id, index) in literatureIds"
+          v-for="(id, index) in displayedLiteratureIds"
           :key="id"
           :class="valueClassMap"
         >
           <NuxtLink :to="`/literature/${id}`">
-            {{ literatureTitles[index] }}
+            {{ displayedLiteratureTitles[index] }}
           </NuxtLink>
         </li>
+        <li
+          v-if="literatureIds.length > 5 && !showAll"
+          class="list-none mt-[-2px]"
+        >
+          <NuxtLink
+            @click.prevent="showAll = true"
+            class="link-button cursor-pointer"
+          >
+            <Icon
+              name="material-symbols:add"
+              class="text-base translate-y-[3px]"
+            />
+            Show more related literature
+          </NuxtLink>
+        </li>
+        <NuxtLink
+          v-if="literatureIds.length > 5 && showAll"
+          class="link-button list-none mt-[-2px] cursor-pointer"
+          @click="showAll = false"
+        >
+          <Icon
+            name="material-symbols:remove"
+            class="text-base translate-y-[3px]"
+          />
+          Show less related literature
+        </NuxtLink>
       </ul>
     </template>
 
@@ -122,6 +149,29 @@ const displayedLiterature = computed(() => {
       : literatureList.value
   }
   return literatureList.value
+})
+
+const displayedLiteratureIds = computed(() => {
+  if (!showAll.value && literatureIds.value.length > 5) {
+    return literatureIds.value.slice(0, 3)
+  }
+  return literatureIds.value
+})
+
+const displayedLiteratureTitles = computed(() => {
+  if (!showAll.value && literatureTitles.value.length > 5) {
+    return literatureTitles.value.slice(0, 3)
+  }
+  return literatureTitles.value
+})
+
+const isLoading = computed(() => {
+  if (props.useId) {
+    return (
+      literatureIds.value.length === 0 || literatureTitles.value.length === 0
+    )
+  }
+  return loading.value
 })
 
 // Function to fetch related literature
