3253e964903c37974f593c5c348102cb5b18d3d7
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-04-08 09:42:28 +0200

    Init landing page card

---
+++ frontend/components/landing-page/RecentDomesticInstruments.vue
@@ -0,0 +1,13 @@
+<template>
+  <UCard class="cold-ucard">
+    <div class="popular-searches-container flex flex-col md:flex-row gap-8">
+      <h2 class="popular-title text-left md:whitespace-nowrap">
+        Recent Domestic Instruments
+      </h2>
+    </div>
+  </UCard>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
