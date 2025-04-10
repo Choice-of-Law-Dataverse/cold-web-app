d375e36ab1134b23ec6ee68b2be6af1dcabcdd9d
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-04-10 14:40:47 +0200

    Fix filter not filtering by jurisdiction

--- frontend/components/search-results/SearchResults.vue
+++ frontend/components/search-results/SearchResults.vue
@@ -165,6 +165,7 @@ const loadJurisdictions = async () => {
       .filter((entry) => entry.Name && entry['Alpha-3 Code'])
       .map((entry) => ({
         label: entry.Name,
+        value: entry.Name, // added property to use in filter
         avatar: {
           src: `https://choiceoflawdataverse.blob.core.windows.net/assets/flags/${entry['Alpha-3 Code'].toLowerCase()}.svg`,
         },
@@ -175,7 +176,7 @@ const loadJurisdictions = async () => {
     )
     // Prepend the default option without an avatar
     jurisdictionOptions.value = [
-      { label: 'All Jurisdictions' },
+      { label: 'All Jurisdictions', value: 'All Jurisdictions' },
       ...mappedJurisdictions,
     ]
   } catch (error) {
@@ -237,13 +238,17 @@ const resetFilters = () => {
 watch(
   [currentJurisdictionFilter, currentThemeFilter, currentTypeFilter],
   ([newJurisdiction, newTheme, newType]) => {
+    const jurisdictionValue =
+      newJurisdiction.length > 0
+        ? newJurisdiction
+            .map((item) => (typeof item === 'object' ? item.value : item))
+            .join(',')
+        : undefined
     const filters = {
-      jurisdiction:
-        newJurisdiction.length > 0 ? newJurisdiction.join(',') : undefined,
+      jurisdiction: jurisdictionValue,
       theme: newTheme.length > 0 ? newTheme.join(',') : undefined,
       type: newType.length > 0 ? newType.join(',') : undefined,
     }
-
     // Only emit if the filters have actually changed
     if (JSON.stringify(filters) !== JSON.stringify(props.filters)) {
       emit('update:filters', filters)
