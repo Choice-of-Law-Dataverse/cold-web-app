3119d09e7ec30922b62fb8d0d00810d060b2d8c9
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-05-09 15:17:53 +0200

    - Add more entries - Clean up code

--- frontend/components/landing-page/RecentDomesticInstruments.vue
+++ frontend/components/landing-page/RecentDomesticInstruments.vue
@@ -43,7 +43,6 @@
 import { ref, onMounted } from 'vue'
 import { useRuntimeConfig } from '#app'
 import { RouterLink } from 'vue-router'
-import { formatDate } from '../../utils/format.js'
 import LoadingLandingPageCard from '../layout/LoadingLandingPageCard.vue'
 
 const domesticInstruments = ref([])
@@ -68,7 +67,7 @@ async function fetchDomesticInstruments() {
     const instrumentsData = await response.json()
     // Convert Date to number, sort descending and take the n most recent
     instrumentsData.sort((a, b) => Number(b.Date) - Number(a.Date))
-    domesticInstruments.value = instrumentsData.slice(0, 5)
+    domesticInstruments.value = instrumentsData.slice(0, 7)
   } catch (error) {
     console.error(error)
     domesticInstruments.value = []
