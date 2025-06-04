11e75db9af38264799a84ab3ac4b2cff6e7aa80e
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-06-04 09:32:28 +0200

    Switch to beta Gtag

--- frontend/plugins/manual-gtag.client.js
+++ frontend/plugins/manual-gtag.client.js
@@ -4,14 +4,14 @@ import { loadGtag } from '@/utils/gtag.js'
 export default defineNuxtPlugin(() => {
   if (process.server) return
   if (localStorage.getItem('cookieConsent') === 'true') {
-    loadGtag('G-ZSYHMWVVRH') // Alpha
-    // loadGtag('G-C61ZX1L3NH') // Beta and live
+    // loadGtag('G-ZSYHMWVVRH') // Alpha
+    loadGtag('G-C61ZX1L3NH') // Beta and live
   }
   // Listen for consent change and load gtag if user allows
   window.addEventListener('storage', (event) => {
     if (event.key === 'cookieConsent' && event.newValue === 'true') {
-      loadGtag('G-ZSYHMWVVRH') // Alpha
-      // loadGtag('G-C61ZX1L3NH') // Beta and Live
+      // loadGtag('G-ZSYHMWVVRH') // Alpha
+      loadGtag('G-C61ZX1L3NH') // Beta and Live
     }
   })
 })
