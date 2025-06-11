d0e51bae574b54ec4db9e9ac893e481395a027f5
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-06-11 17:52:32 +0200

    Adjust paths

--- frontend/components/ui/BaseDetailDisplay.vue
+++ frontend/components/ui/BaseDetailDisplay.vue
@@ -144,7 +144,7 @@ import BackButton from '~/components/ui/BackButton.vue'
 import BaseCardHeader from '~/components/ui/BaseCardHeader.vue'
 import NotificationBanner from '~/components/ui/NotificationBanner.vue'
 import LoadingCard from '@/components/layout/LoadingCard.vue'
-import InfoTooltip from './InfoTooltip.vue'
+import InfoTooltip from '@/components/ui/InfoTooltip.vue'
 
 // Tooltips for Question Page
 import tooltipQuestion from '@/content/info_boxes/question/question.md?raw'
