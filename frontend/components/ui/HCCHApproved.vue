f2cf532d20c66abad5557b30df8bfc7031c500a4
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-07-01 16:24:59 +0200

    Adjust style

--- frontend/components/ui/HCCHApproved.vue
+++ frontend/components/ui/HCCHApproved.vue
@@ -1,5 +1,17 @@
 <template>
-  <span>HCCH Approved</span>
+  <span class="label-question">
+    <UIcon
+      name="i-material-symbols:check-circle"
+      class="-mb-[0.175em] mr-[0.1em] text-[1.15em] -ml-[0.1em]"
+    />
+    HCCH Approved
+  </span>
 </template>
 
-<script setup></script>
+<style scoped>
+.label-question {
+  color: var(--color-cold-night);
+  background-color: var(--color-cold-green);
+  border: 1px solid var(--color-cold-night);
+}
+</style>
