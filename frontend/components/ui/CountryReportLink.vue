c7c4de9c466c806f3c3f44cef1bcb0e671bf961f
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-07-03 10:15:34 +0200

    Streamline code

--- frontend/components/ui/CountryReportLink.vue
+++ frontend/components/ui/CountryReportLink.vue
@@ -6,9 +6,9 @@
           <div class="popular-searches-container flex flex-col gap-8">
             <!-- Title Section -->
             <div>
-              <h2 class="popular-title text-left md:whitespace-nowrap">
-                Link to Country Report
-              </h2>
+              <h3 class="text-left md:whitespace-nowrap">
+                Go to the country report for [jurisdiction]
+              </h3>
             </div>
           </div>
         </UCard>
@@ -19,18 +19,4 @@
 
 <script setup></script>
 
-<style scoped>
-.suggestions {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem; /* Adjust spacing between buttons */
-}
-
-.suggestion-button {
-  max-width: 100%; /* Ensure the button doesn’t overflow */
-  white-space: normal; /* Allow text wrapping */
-  text-align: left; /* Align text to the left */
-  padding: 0.5rem 1rem; /* Adjust padding for better fit */
-  word-break: break-word; /* Break long words */
-}
-</style>
+<style scoped></style>
