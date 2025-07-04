0cb39ad076b9b4bd4af87288f6529b99f6c61dee
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-07-04 14:02:24 +0200

    Init compatible label

---
+++ frontend/components/ui/CompatibleLabel.vue
@@ -0,0 +1,18 @@
+<template>
+  <span
+    class="label-question"
+    style="display: inline-flex; width: 126px; align-items: center"
+  >
+    <UIcon name="i-material-symbols:balance" class="text-[3em] mr-2" />
+    UNCITRAL Model Law
+  </span>
+</template>
+
+<style scoped>
+.label-question {
+  color: var(--color-cold-night);
+  background-color: var(--color-cold-green-alpha);
+  border: 1px solid var(--color-cold-green);
+  line-height: 1.2em;
+}
+</style>
