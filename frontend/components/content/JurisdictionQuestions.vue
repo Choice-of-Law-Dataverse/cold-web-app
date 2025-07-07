d2cde98915bdccc775438696d9c235a1619cbdfb
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-07-07 09:02:57 +0200

    Adjust style and add example content to table

--- frontend/components/content/JurisdictionQuestions.vue
+++ frontend/components/content/JurisdictionQuestions.vue
@@ -4,8 +4,10 @@
       <div class="col-span-12">
         <UCard class="cold-ucard">
           <div>
-            <h2>Questions</h2>
-            <UTable :rows="people" />
+            <h2 class="mb-10">Questions</h2>
+            <div class="result-value-small table-full-width-wrapper">
+              <UTable :rows="people" />
+            </div>
           </div>
         </UCard>
       </div>
@@ -16,46 +18,61 @@
 <script setup>
 const people = [
   {
-    id: 1,
-    name: 'Lindsay Walton',
-    title: 'Front-end Developer',
-    email: 'lindsay.walton@example.com',
-    role: 'Member',
+    question: 'Is there a codification on choice of law?',
+    answer: 'Yes',
   },
   {
-    id: 2,
-    name: 'Courtney Henry',
-    title: 'Designer',
-    email: 'courtney.henry@example.com',
-    role: 'Admin',
+    question:
+      'Do the courts have the authority to refer to the HCCH Principles as persuasive authority?',
+    answer: 'Yes',
   },
   {
-    id: 3,
-    name: 'Tom Cook',
-    title: 'Director of Product',
-    email: 'tom.cook@example.com',
-    role: 'Member',
+    question:
+      'Is the principle of party autonomy in respect of choice of law in international commercial contracts widely accepted in this jurisdiction?',
+    answer: 'No',
   },
   {
-    id: 4,
-    name: 'Whitney Francis',
-    title: 'Copywriter',
-    email: 'whitney.francis@example.com',
-    role: 'Admin',
-  },
-  {
-    id: 5,
-    name: 'Leonard Krasner',
-    title: 'Senior Designer',
-    email: 'leonard.krasner@example.com',
-    role: 'Owner',
-  },
-  {
-    id: 6,
-    name: 'Floyd Miles',
-    title: 'Principal Designer',
-    email: 'floyd.miles@example.com',
-    role: 'Member',
+    question:
+      'More specifically, are the parties to an international commercial contract allowed to choose the law applicable to their contract?',
+    answer: 'No',
   },
 ]
 </script>
+
+<style scoped>
+.result-value-small,
+.result-value-small :deep(td),
+.result-value-small :deep(th),
+.result-value-small :deep(tr),
+.result-value-small :deep(span),
+.result-value-small :deep(div) {
+  color: var(--color-cold-night) !important;
+}
+
+.table-full-width-wrapper {
+  /* Remove padding from the UCard for this table only */
+  margin-left: calc(-1 * var(--card-padding, 1.5rem));
+  margin-right: calc(-1 * var(--card-padding, 1.5rem));
+  width: calc(100% + 2 * var(--card-padding, 1.5rem));
+  margin-bottom: -1.55rem !important;
+}
+.table-full-width-wrapper :deep(table) {
+  width: 100% !important;
+  table-layout: auto;
+}
+
+.table-full-width-wrapper :deep(tr) {
+  height: 72px;
+  min-height: 72px;
+}
+
+.table-full-width-wrapper :deep(thead) {
+  display: none;
+}
+
+.table-full-width-wrapper :deep(th),
+.table-full-width-wrapper :deep(td) {
+  border-bottom: 1px solid var(--color-cold-gray) !important;
+  border-top: 1px solid var(--color-cold-gray) !important;
+}
+</style>
