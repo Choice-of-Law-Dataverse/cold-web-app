20ee77ba29eeec9448c25438de1eda8a7b47b4f7
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-07-14 08:32:43 +0200

    Init empty page

---
+++ frontend/pages/jurisdiction-comparison/JurisdictionComparison.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="px-6">
+    <div class="mx-auto" style="max-width: var(--container-width); width: 100%">
+      <BaseDetailLayout
+        :loading="false"
+        :resultData="{}"
+        :keyLabelPairs="[]"
+        :valueClassMap="{}"
+        sourceTable=""
+      />
+    </div>
+  </main>
+</template>
+
+<script setup>
+import BaseDetailLayout from '@/components/layouts/BaseDetailLayout.vue'
+</script>
