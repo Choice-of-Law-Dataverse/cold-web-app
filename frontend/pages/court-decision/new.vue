c7e32ff9391cb4fc88a02cd39c0f413c90f53ad3
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2025-09-10 15:46:10 +0200

    Fix typo

--- frontend/pages/court-decision/new.vue
+++ frontend/pages/court-decision/new.vue
@@ -20,7 +20,7 @@
         target="_blank"
         rel="noopener noreferrer"
       >
-        Open the Case Analyze now
+        Open the Case Analyzer now
         <UIcon
           name="i-material-symbols:open-in-new"
           class="relative top-[2px]"
