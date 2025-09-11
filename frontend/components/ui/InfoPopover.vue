fa8350ef3c4d5e747bf786be462dcc0170a38155
Marcos Alexandre Figur Messer <marcos.messer@driven.world>
2025-09-11 19:02:39 +0200

    fix: use popover instead

---
+++ frontend/components/ui/InfoPopover.vue
@@ -0,0 +1,47 @@
+<template>
+  <UPopover
+    :popper="{ placement }"
+    :ui="{
+      background: 'bg-cold-night',
+      ring: '',
+      rounded: 'rounded-none',
+      shadow: 'shadow-lg',
+      base: 'overflow-hidden',
+    }"
+  >
+    <span class="icon cursor-pointer p-2">
+      <Icon name="i-material-symbols:info-outline" />
+    </span>
+
+    <template #panel>
+      <div class="content p-3">
+        {{ text }}
+      </div>
+    </template>
+  </UPopover>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  placement: {
+    type: String,
+    default: 'top',
+  },
+})
+</script>
+
+<style scoped>
+.icon {
+  color: var(--color-cold-purple);
+}
+
+.content {
+  background-color: var(--color-cold-night);
+  color: white;
+  text-transform: none;
+}
+</style>
