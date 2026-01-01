061a2f01ad823c87c43698ac527325855cb5a63f
Marcos Alexandre Figur Messer <git@marcosmesser.com>
2026-01-01 16:14:59 +0100

    fix: multi stage image

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
   nitro: {
+    prerender: {
+      crawlLinks: true,
+    },
     experimental: {
       tasks: false,
     },
